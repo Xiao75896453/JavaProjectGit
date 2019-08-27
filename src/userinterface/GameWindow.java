d1b557ac2b52664c074ded39ce426edb65964458
xiao75896453 <a75896453@mail.com>
2019-08-27 15:34:38 +0800

    Create New Class "GameBackgroundMusic"

--- src/userinterface/GameWindow.java
+++ src/userinterface/GameWindow.java
@@ -36,37 +36,9 @@ public class GameWindow extends JFrame {
 	}
 	
 	public static void main(String args[])throws Exception {
-		Clip clip = createClip(new File("data/BGM.wav"));
+		GameBackgroundMusic BGM = new GameBackgroundMusic();
+		BGM.setFile(new File("data/BGM.wav")).start();
 		(new GameWindow()).startGame();
-		clip.start();
 		Thread.sleep(3000);
-	}
-	public static Clip createClip(File path) {
-		//指定されたURLのオーディオ入力ストリームを取得
-		try (AudioInputStream ais = AudioSystem.getAudioInputStream(path)){
-			
-			//ファイルの形式取得
-			AudioFormat af = ais.getFormat();
-			
-			//単一のオーディオ形式を含む指定した情報からデータラインの情報オブジェクトを構築
-			DataLine.Info dataLine = new DataLine.Info(Clip.class,af);
-			
-			//指定された Line.Info オブジェクトの記述に一致するラインを取得
-			Clip c = (Clip)AudioSystem.getLine(dataLine);
-			
-			//再生準備完了
-			c.open(ais);
-			
-			return c;
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (UnsupportedAudioFileException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (LineUnavailableException e) {
-			e.printStackTrace();
-		}
-		return null;
 	}
 }
